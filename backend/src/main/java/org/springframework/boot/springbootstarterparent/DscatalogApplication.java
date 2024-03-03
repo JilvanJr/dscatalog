0706b89f7e740daf0e1a33f10af1f61b5c14cc98
Jilvan Junior <jilvanj@hotmail.com>
2024-03-03 16:36:14 -0300

    Backend project created

---
+++ backend/src/main/java/org/springframework/boot/springbootstarterparent/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.springbootstarterparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+
+}
