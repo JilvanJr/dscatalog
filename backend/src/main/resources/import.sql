e439b5c09e68ac31009840341f074c60f4d2c4b0
Jilvan Junior <jilvanj@hotmail.com>
2024-03-27 00:39:31 -0300

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,28 @@
-INSERT INTO tb_category(name) VALUES ('Books')
-INSERT INTO tb_category(name) VALUES ('Electronics')
-INSERT INTO tb_category(name) VALUES ('Computers')
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Electronics');
+INSERT INTO tb_category(name) VALUES ('Computers');
+INSERT INTO tb_category(name) VALUES ('The Lord of the Rings');
+INSERT INTO tb_category(name) VALUES ('Smart TV');
+INSERT INTO tb_category(name) VALUES ('Macbook Pro');
+INSERT INTO tb_category(name) VALUES ('PC Gamer');
+INSERT INTO tb_category(name) VALUES ('Rails for Dummies');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Ex');
+INSERT INTO tb_category(name) VALUES ('PC Gamer X');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Alfa');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Tera');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Y');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Nitro');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Card');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Plus');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Hera');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Weed');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Max');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Turbo');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Hot');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Ez');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Tr');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Tx');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Er');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Min');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Boo');
+INSERT INTO tb_category(name) VALUES ('PC Gamer Foo');
